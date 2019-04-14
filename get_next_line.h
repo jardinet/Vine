1ab591dfc022bdb9244f9f61d2e729f1ca7776fe
Marie WRAGG <mwragg@student.42.fr>
2019-04-14 23:43:07 +0200

    coucou Leo

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mwragg <mwragg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 10:19:09 by mwragg            #+#    #+#             */
-/*   Updated: 2019/04/14 22:35:45 by mwragg           ###   ########.fr       */
+/*   Updated: 2019/04/14 22:40:52 by mwragg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "./Libft/libft.h"
-# define BUFF_SIZE 10
+# define BUFF_SIZE 4 
+# define CUT_CHAR '\n'
 
 typedef struct	s_fd
 {
 	int			fd;
 	int			ret;
 	char		buf[BUFF_SIZE];
-	int		len;
+	int			len;
+	int			start;
 }				t_fd;
 
 int		get_next_line(const int fd, char **line);
